6a00beaef4f363f1403528652ded801e7cb1a9cd
Deseteral <deseteral@gmail.com>
2022-10-02 18:08:21 +0200

    Moved tables code to separate files

---
+++ src/game/tables/client-table.ts
@@ -0,0 +1,77 @@
+import { Engine } from 'src/engine/engine';
+import { Font } from 'src/engine/font';
+import { drawFrame } from 'src/engine/frame';
+import { Input } from 'src/engine/input';
+import { Textures } from 'src/engine/textures';
+import { Recipe } from 'src/game/recipes';
+import { Table } from 'src/game/tables/table';
+
+export class ClientTable extends Table {
+  nextClientAtTicks: number = 10 * 60;
+
+  update(isSelected: boolean): void {
+    if (Engine.ticks >= this.nextClientAtTicks) {
+      const recipeRange: number = (Engine.state.completedOrders <= 3) ? 5 : (Engine.state.recipes.length - 1);
+      const recipeIdx: number = Math.randomRange(0, recipeRange);
+      const recipe = Engine.state.recipes[recipeIdx];
+      Engine.state.orders.push(recipe);
+
+      this.nextClientAtTicks = Engine.ticks + (60 * 10); // I assume the game is running at 60 fps so we can use that to measure time, it's stupid but will be easier to implement pause.
+
+      console.log('new client with order', recipe);
+    }
+
+    if (Input.getKeyDown('right') && isSelected) this.onNextTableCb();
+    if (Input.getKeyDown('down') && isSelected) this.openBook();
+  }
+
+  render(ctx: CanvasRenderingContext2D): void {
+    drawFrame(11, 11, 100, 218, ctx, () => {
+      Font.draw('Orders', 12, 6, ctx);
+
+      for (let idx = 0; idx < Engine.state.orders.length; idx += 1) {
+        const orderRecipe: Recipe = Engine.state.orders[idx];
+
+        const yy: number = Font.glyphSizeV + idx * (16 + 4);
+        Font.draw(orderRecipe.name, 11, yy, ctx);
+      }
+    });
+
+    drawFrame(11 + 118, 11, 260, 16, ctx, () => {
+      // Time counter
+      const secondsUnitlNextClient: number = 10 - Math.round((this.nextClientAtTicks - Engine.ticks) / 60);
+
+      for (let tidx = 0; tidx < 10; tidx += 1) {
+        const size = 5;
+        const xx = 11 + 118 + 10 + (tidx * (size + 2));
+        const yy = 17;
+        if (tidx < secondsUnitlNextClient) {
+          ctx.fillRect(xx, yy, size, size);
+        } else {
+          ctx.drawRect(xx, yy, size, size);
+        }
+      }
+
+      // Gold
+      ctx.drawImage(Textures.coinTexture.normal, 300, 11);
+      Font.draw(Engine.state.gold.toString(), 300 + 16 + 2, 5, ctx);
+    });
+
+    // max line len 37
+    const messageFrameWidth: number = 260;
+    drawFrame(11 + 118, 11 + 34, messageFrameWidth, 184, ctx, () => {
+      let line = 0;
+      [...Engine.state.messageBoard.messages].reverse().forEach((message) => {
+        const basexx: number = 11 + 118;
+
+        [...message.text].reverse().forEach((txt) => {
+          const xx: number = message.rightSide ? (basexx + messageFrameWidth - Font.lineLengthPx(txt, true) + 15) : basexx;
+          const yy: number = 205 - (line * ((Font.glyphSizeV / 3) | 0));
+          Font.draw(txt, xx, yy, ctx, true);
+
+          line += 1;
+        });
+      });
+    });
+  }
+}
---
+++ src/game/tables/ingredients-table.ts
@@ -0,0 +1,125 @@
+import { Engine } from 'src/engine/engine';
+import { Font } from 'src/engine/font';
+import { drawFrame } from 'src/engine/frame';
+import { Input } from 'src/engine/input';
+import { Textures, Texture } from 'src/engine/textures';
+import { Ingredient, Ingredients, IngredientAction, ingredientDisplayName } from 'src/game/ingredients';
+import { getIngredientIcon } from 'src/game/recipes';
+import { BurningStation } from 'src/game/stations/burning-station';
+import { CuttingStation } from 'src/game/stations/cutting-station';
+import { EnchantmentStation } from 'src/game/stations/enchantment-station';
+import { GrindingStation } from 'src/game/stations/grinding-station';
+import { Station, StationCompleteCallback } from 'src/game/stations/station';
+import { Table } from 'src/game/tables/table';
+
+export class IngredientsTable extends Table {
+  selectedStation: number = 0;
+  activeStation: (Station | null) = null;
+
+  isIndredientPickerOpen: boolean = false;
+  ingredientCursor: number = 0;
+
+  ignoringInputTicks = 0;
+
+  update(isSelected: boolean): void {
+    Engine.shouldCountTicks = !this.activeStation;
+
+    if (this.activeStation && isSelected) {
+      this.activeStation.update();
+      return;
+    }
+
+    this.ignoringInputTicks -= 1;
+
+    if (!this.isIndredientPickerOpen && Input.getKeyDown('a') && this.canUseInput(isSelected)) {
+      this.isIndredientPickerOpen = true;
+      return;
+    }
+
+    if (this.isIndredientPickerOpen && isSelected) {
+      if (Input.getKeyDown('up')) this.ingredientCursor -= 1;
+      if (Input.getKeyDown('down')) this.ingredientCursor += 1;
+      if (Input.getKeyDown('b')) {
+        this.isIndredientPickerOpen = false;
+        return;
+      }
+      this.ingredientCursor = Math.clamp(this.ingredientCursor, 0, 5 - 1);
+
+      if (Input.getKeyDown('a')) {
+        const selectedIngredient: Ingredient = Ingredients[this.ingredientCursor];
+        this.ingredientCursor = 0;
+        this.isIndredientPickerOpen = false;
+
+        const cb: StationCompleteCallback = (success: boolean, action: IngredientAction) => {
+          if (success) {
+            Engine.state.preparedIngredients.push({ ingredient: selectedIngredient, action });
+          }
+          this.exitStation();
+        };
+
+        if (this.selectedStation === 0) {
+          this.activeStation = new CuttingStation(cb);
+        } else if (this.selectedStation === 1) {
+          this.activeStation = new GrindingStation(cb);
+        } else if (this.selectedStation === 2) {
+          this.activeStation = new BurningStation(cb);
+        } else if (this.selectedStation === 3) {
+          this.activeStation = new EnchantmentStation(cb);
+        }
+      }
+
+      return;
+    }
+
+    if (Input.getKeyDown('right') && this.canUseInput(isSelected)) this.selectedStation += 1;
+    if (Input.getKeyDown('left') && this.canUseInput(isSelected)) this.selectedStation -= 1;
+
+    if (this.selectedStation < 0) {
+      this.onPreviousTableCb();
+    } else if (this.selectedStation > 3) {
+      this.onNextTableCb();
+    } else if (Input.getKeyDown('down') && this.canUseInput(isSelected)) {
+      this.openBook();
+    }
+
+    this.selectedStation = Math.clamp(this.selectedStation, 0, 3);
+  }
+
+  render(ctx: CanvasRenderingContext2D): void {
+    ctx.drawImage(Textures.tableTexture.normal, 0, 0);
+
+    this.drawStation(Textures.cuttingTexture, 10, 40, this.selectedStation === 0, ctx);
+    this.drawStation(Textures.grindingTexture, 106, 93, this.selectedStation === 1, ctx);
+    this.drawStation(Textures.burningTexture, 190, 25, this.selectedStation === 2, ctx);
+    this.drawStation(Textures.enchantingTexture, 305, 100, this.selectedStation === 3, ctx);
+
+    if (this.isIndredientPickerOpen) {
+      drawFrame(11, 11, 120, 97, ctx, () => {
+        Ingredients.forEach((ing, idx) => {
+          const xx: number = 11;
+          const yy: number = 6 + idx * (Font.glyphSizeV / 2);
+          if (idx === this.ingredientCursor) ctx.drawImage(Textures.listPointerRightTexture.normal, xx, yy + 5);
+          ctx.drawImage(getIngredientIcon(ing), xx + 16, yy + 5);
+          Font.draw(`${ingredientDisplayName(ing)}`, xx + 16 + 16 + 2, yy, ctx);
+        });
+      });
+    }
+
+    if (this.activeStation) this.activeStation.render(ctx);
+  }
+
+  private drawStation(texture: Texture, x: number, y: number, isSelected: boolean, ctx: CanvasRenderingContext2D): void {
+    const frameOffset = 4;
+    if (isSelected) ctx.drawRect(x - frameOffset, y - frameOffset, texture.normal.width + frameOffset * 2, texture.normal.height + frameOffset * 2);
+    ctx.drawImage(texture.normal, x, y);
+  }
+
+  private exitStation(): void {
+    this.activeStation = null;
+    this.ignoringInputTicks = (1.5 * 60) | 0;
+  }
+
+  private canUseInput(isSelected: boolean): boolean {
+    return this.ignoringInputTicks <= 0 && isSelected;
+  }
+}
